661b74acfeb50bc20c96487e87e142ff3eb2e546
Conrado Peloso <conradopeloso.cp@gmail.com>
2025-02-26 15:52:37 -0300

    create Enum.java and update audiencias

---
+++ Proyecto Audiencias/backend/src/main/java/com/example/prueba2/Enums/Enum.java
@@ -0,0 +1,10 @@
+package com.example.prueba2.Enums;
+
+public class Enum {
+
+    public enum EstadoAudiencia {
+        DEMORADA, EN_HORARIO, REALIZADA, SUSPENDIDA;
+    }
+    
+    
+}
--- Proyecto Audiencias/backend/src/main/java/com/example/prueba2/models/Audiencia.java
+++ Proyecto Audiencias/backend/src/main/java/com/example/prueba2/models/Audiencia.java
@@ -1,6 +1,16 @@
 package com.example.prueba2.models;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import com.example.prueba2.Enums.Enum.EstadoAudiencia;
+
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -13,17 +23,34 @@ public class Audiencia {
     @Id
     private Integer aud_id;
     private String aud_nombre;
-    //private dateTime aud_fecins;
-    //private dateTime aud_fecmod;
+    private LocalDateTime aud_fecins;
+    private LocalDateTime aud_fecmod;
     //private enum aud_estado;
     //private enum aud_tipo;
-    //private date fecha
-    //private time aud_hora;
-    //private time aud_duracion;
-    //private Integer aud_cuij;
-    //private String aud_caratula;
+    private LocalDate fecha;
+    private LocalTime aud_hora;
+    private LocalTime aud_duracion;
+    private Integer aud_cuij;
+    private String aud_caratula;
 
    @ManyToOne
-   @JoinColumn(name="usr_id")
-    private Usuario usuario;
+   @JoinColumn(name="sal_id")
+    private Sala sal_id;
+
+    @ManyToOne
+    @JoinColumn(name = "aud_usrins", nullable = false) // Usuario que la creó
+    private Usuario usuarioCreacion;
+
+    @ManyToOne
+    @JoinColumn(name = "aud_usrmod") // Usuario que la modificó (puede ser null)
+    private Usuario usuarioModificacion;
+
+    @Enumerated(EnumType.STRING) // Guarda el nombre del enum en la BD
+    @Column(name = "aud_tipo")
+    private EstadoAudiencia aud_tipo;
+
+    @Enumerated(EnumType.STRING) // Guarda el nombre del enum en la BD
+    @Column(name = "aud_estado")
+    private EstadoAudiencia aud_estado;
+
 }
