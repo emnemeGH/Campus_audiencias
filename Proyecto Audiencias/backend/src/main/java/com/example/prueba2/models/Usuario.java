29a77f0c0f3dcccb005e798754e9490a0825c56c
Conrado Peloso <conradopeloso.cp@gmail.com>
2025-03-02 16:11:06 -0300

    update usuario.java

--- Proyecto Audiencias/backend/src/main/java/com/example/prueba2/models/Usuario.java
+++ Proyecto Audiencias/backend/src/main/java/com/example/prueba2/models/Usuario.java
@@ -1,6 +1,8 @@
 package com.example.prueba2.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -11,17 +13,16 @@ import lombok.Data;
 
 public class Usuario {
     
-    //Atributos de la clase Usuario
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer usr_id;
     private String usr_nombre;
     private String usr_username;
     private String usr_mail;
-    //private Enum usr_estado;
+    private Boolean usr_estado;
     private Integer usr_login;
     private String usr_password;
     private Integer usr_isadmin;
-    //private Interger dis_id;  
     
     @ManyToOne
     @JoinColumn(name="dis_id")
