e1e494deba9fc51baa3bc35ae3a9b1d66852e030
Gianluca D. <gianludane.6@gmail.com>
2025-03-01 22:24:51 -0300

    funcion para listar audiencias personalizadas

--- Proyecto Audiencias/frontend/src/app/crear-audiencia/crear-audiencia.component.ts
+++ Proyecto Audiencias/frontend/src/app/crear-audiencia/crear-audiencia.component.ts
@@ -6,5 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./crear-audiencia.component.css']
 })
 export class CrearAudienciaComponent {
-
 }
--- Proyecto Audiencias/frontend/src/app/lista-audiencias/lista-audiencias.component.ts
+++ Proyecto Audiencias/frontend/src/app/lista-audiencias/lista-audiencias.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AudienciaService } from '../audiencia.service';
 @Component({
   selector: 'app-lista-audiencias',
   templateUrl: './lista-audiencias.component.html',
   styleUrls: ['./lista-audiencias.component.css']
 })
-export class ListaAudienciasComponent {
+export class ListaAudienciasComponent implements OnInit{
 
+
+  constructor(private audienciaService: AudienciaService){}
+
+  audiencias: { cuij: string; caratula: string; tipo: string }[] = []; 
+  
+  ngOnInit() {
+    this.audiencias = [
+      { cuij: '21087069726', caratula: 'MARTINEZ s/Homicidio doloso simple y otros', tipo: 'Juicio de querella' },
+      { cuij: '21087069727', caratula: 'PEREZ s/Robo agravado', tipo: 'Juicio oral' },
+      { cuij: '21087069728', caratula: 'GOMEZ s/Estafa', tipo: 'Audiencia preliminar' },
+    ];
+  }
   editarAudiencia(){
     console.log("audiencia editada");
   }
