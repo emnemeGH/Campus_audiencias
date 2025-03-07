d6374ac3015f9e5baea80da7222d6c8177b0f1b9
Conrado Peloso <conradopeloso.cp@gmail.com>
2025-03-07 13:30:12 -0300

    intento de validacion de autoridades con audicencias
    falta terminar

--- Proyecto Audiencias/backend/src/main/java/com/example/prueba2/controller/Audiencia_extController.java
+++ Proyecto Audiencias/backend/src/main/java/com/example/prueba2/controller/Audiencia_extController.java
@@ -1,6 +1,9 @@
 package com.example.prueba2.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +15,23 @@ import com.example.prueba2.services.Audiencia_extService;
 @CrossOrigin(origins = "http://localhost:4200")
 public class Audiencia_extController extends BaseController<Audiencia_ext, Integer> {
 
-    public Audiencia_extController(Audiencia_extService service) {
-        super(service);
+    private final Audiencia_extService Audiencia_extService;
+
+    public Audiencia_extController(Audiencia_extService Audiencia_extService) {
+        super(Audiencia_extService);
+        this.Audiencia_extService = Audiencia_extService;
     }
+
+    @PostMapping("/api/audienciasExt")
+    public ResponseEntity<?> crearAudienciaExt(@RequestBody Audiencia_ext audienciaExt) {
+        try {
+            Audiencia_ext nuevaAudienciaExt = Audiencia_extService.guardar(audienciaExt);
+            return ResponseEntity.ok(nuevaAudienciaExt);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());  // 🔹 Devuelve el error si hay conflicto
+        }
+    }
+
+    
+
 }
--- Proyecto Audiencias/backend/src/main/java/com/example/prueba2/repository/Audiencia_extRepository.java
+++ Proyecto Audiencias/backend/src/main/java/com/example/prueba2/repository/Audiencia_extRepository.java
@@ -1,12 +1,21 @@
 package com.example.prueba2.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.prueba2.models.Audiencia_ext;
 
 @Repository
-
 public interface Audiencia_extRepository extends JpaRepository<Audiencia_ext, Integer>{
     
+    @Query("SELECT ae FROM Audiencia_ext ae " +
+           "JOIN ae.aud_id a " +
+           "WHERE ae.aut_id.aut_id = :autoridadId " +
+           "AND a.aud_fecha = :fecha " +
+           "AND a.aud_hora = :hora")
+    List<Audiencia_ext> encontrarConflictos(Integer eau_id, LocalDate eau_fecins);
 }
