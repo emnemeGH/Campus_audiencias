3588566b0f6b71c422033187d943b88fb102edc7
emnemeGH <emaneme05@gmail.com>
2025-03-11 19:53:35 -0300

    Traer autoridades por id de la audiencia, back

--- Proyecto Audiencias/backend/src/main/java/com/example/prueba2/services/AudienciaService.java
+++ Proyecto Audiencias/backend/src/main/java/com/example/prueba2/services/AudienciaService.java
@@ -4,11 +4,15 @@ import com.example.prueba2.services.impl.BaseServiceImpl;
 
 import jakarta.transaction.Transactional;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.prueba2.models.Audiencia;
+import com.example.prueba2.models.Audiencia_ext;
 import com.example.prueba2.repository.AudienciaRepository;
+import com.example.prueba2.repository.Audiencia_extRepository;
 
 @Service
 public class AudienciaService extends BaseServiceImpl<Audiencia, Integer> {
@@ -24,5 +28,12 @@ public class AudienciaService extends BaseServiceImpl<Audiencia, Integer> {
     @Transactional
     public void borradoLogico(Integer id) {
         audienciaRepository.borrarLogico(id);
+    }
+
+    @Autowired
+    private Audiencia_extRepository Audiencia_extRepository;
+
+    public List<Audiencia_ext> obtenerAutoridadesPorAudiencia(Long aud_id) {
+        return Audiencia_extRepository.findByAudienciaId(aud_id);
     }
 }
