ecf1175469bedd2fc9b0a75036a8cf7a2696defb
Conrado Peloso <conradopeloso.cp@gmail.com>
2025-03-15 01:16:00 -0300

    update usuarioController y creacion usuarioDTO

--- Proyecto Audiencias/backend/src/main/java/com/example/prueba2/controller/UsuarioController.java
+++ Proyecto Audiencias/backend/src/main/java/com/example/prueba2/controller/UsuarioController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.prueba2.dto.RegistroUsuarioDTO;
 import com.example.prueba2.models.Usuario;
 import com.example.prueba2.services.UsuarioService;
 
@@ -38,20 +39,19 @@ public class UsuarioController extends BaseController<Usuario, Integer>{
     }
 
     @PostMapping("/registrar")
-    public ResponseEntity<?> registrarUsuario(@RequestBody Usuario usuario, @RequestParam Integer usuarioSolicitanteId) {
-        Usuario usuarioSolicitante = usuarioService.obtenerPorId(usuarioSolicitanteId)
-            .orElseThrow(() -> new IllegalArgumentException("Usuario solicitante no encontrado"));
-    
-        // ❌ Si no es admin, no puede crear usuarios
-        if (!usuarioSolicitante.getUsrIsAdmin()) {
-            return ResponseEntity.status(403).body("No tienes permisos para crear usuarios.");
-        }
-    
-        try {
-            Usuario nuevoUsuario = usuarioService.registrarUsuario(usuario);
-            return ResponseEntity.ok(nuevoUsuario);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+    public ResponseEntity<?> registrarUsuario(@RequestBody RegistroUsuarioDTO request) {
+    Integer usuarioSolicitanteId = request.getUsuarioSolicitanteId();
+    Usuario usuario = request.getUsuario();
+
+    Usuario usuarioSolicitante = usuarioService.obtenerPorId(usuarioSolicitanteId)
+        .orElseThrow(() -> new IllegalArgumentException("Usuario solicitante no encontrado"));
+
+    if (!usuarioSolicitante.getUsrIsAdmin()) {
+        return ResponseEntity.status(403).body("No tienes permisos para crear usuarios.");
     }
+
+    Usuario nuevoUsuario = usuarioService.registrarUsuario(usuario);
+    return ResponseEntity.ok(nuevoUsuario);
+}
+
 }
