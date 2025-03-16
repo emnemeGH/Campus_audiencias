0b4585fd8d76ac209cc6501910c5cd576c828249
Conrado Peloso <conradopeloso.cp@gmail.com>
2025-03-16 14:28:47 -0300

    principio de validacion de salas con audiencias

--- Proyecto Audiencias/backend/src/main/java/com/example/prueba2/repository/AudienciaRepository.java
+++ Proyecto Audiencias/backend/src/main/java/com/example/prueba2/repository/AudienciaRepository.java
@@ -7,8 +7,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.example.prueba2.models.Audiencia;
+import com.example.prueba2.models.Sala;
 
 import jakarta.transaction.Transactional;
 
@@ -26,11 +28,23 @@ public interface AudienciaRepository extends JpaRepository<Audiencia, Integer>{
     //List<Audiencia> encontrarActivas();
     void borrarLogico(Integer id);
 
-    //Este método verifica si la autoridad ya tiene una audiencia en la misma fecha y hora.
+    // Encuentra todas las audiencias de una sala específica
+    @Query("SELECT a FROM Audiencia a WHERE a.sal_id.sal_id = :sal_id")
+    List<Audiencia> encontrarPorSala(@Param("sal_id") Integer sal_id);
+
+    // Obtiene todas las salas que tienen al menos una audiencia asignada
+    @Query("SELECT DISTINCT a.sal_id FROM Audiencia a")
+    List<Sala> encontrarSalasConAudiencias();
+
+    // Cuenta cuántas audiencias tienen la misma autoridad en la misma fecha y hora
     @Query("SELECT COUNT(a) FROM Audiencia a " +
-       "JOIN Audiencia_ext ae ON ae.audiencia.aud_id = a.aud_id " +
-       "WHERE ae.autoridad.aut_id = :autoridadId " +
-       "AND a.aud_fecha = :fecha " +
-       "AND a.aud_hora = :hora")
-    Long contarConflictos(Integer autoridadId, LocalDate fecha, LocalTime hora);
+           "JOIN Audiencia_ext ae ON ae.audiencia.aud_id = a.aud_id " +
+           "WHERE ae.autoridad.aut_id = :autoridadId " +
+           "AND a.aud_fecha = :fecha " +
+           "AND a.aud_hora = :hora")
+    Long contarConflictos(@Param("autoridadId") Integer autoridadId, 
+                          @Param("fecha") LocalDate fecha, 
+                          @Param("hora") LocalTime hora);
+
+
 }
