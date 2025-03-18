c6f7b6b7f0af0de3c482eecaf74f047987894166
emnemeGH <emaneme05@gmail.com>
2025-03-18 16:04:33 -0300

    Filtros por fiscal, juez y estado

--- Proyecto Audiencias/frontend/src/app/lista-audiencias/lista-audiencias.component.ts
+++ Proyecto Audiencias/frontend/src/app/lista-audiencias/lista-audiencias.component.ts
@@ -12,14 +12,31 @@ export class ListaAudienciasComponent implements OnInit {
   distritoSeleccionado: string = '';
   salaSeleccionada: string = '';
   salasDisponibles: string[] = [];
+  fiscalSeleccionado: string = '';
+  fiscalesDisponibles: string[] = [];
+  juezSeleccionado: string = '';
+  juecesDisponibles: string[] = [];
   audienciasFiltradas: any[] = [];
   fechaSeleccionada: string = '';
+  estadoSeleccionado: string = '';
 
   salasPorDistrito: { [key: string]: string[] } = {
     "Distrito Capital": ["Sala Penal 1", "Sala Civil 1", "Sala Familia 1", "Cámara Gessel 1"],
     "Distrito Sur": ["Sala Penal 2", "Sala Civil 2", "Sala Familia 2", "Cámara Gessel 2"],
     "Distrito Norte": ["Sala Penal 3", "Sala Civil 3", "Sala Familia 3", "Cámara Gessel 3"]
   };
+
+  juecesPorDistrito: { [key: string]: string[] } = {
+    "Distrito Capital": ["Juez1 Dist1", "Juez2 Dist1"],
+    "Distrito Sur": ["Juez1 Dist2", "Juez2 Dist2"],
+    "Distrito Norte": ["Juez1 Dist3", "Juez2 Dist3"],
+  }
+
+  fiscalesPorDistrito: { [key: string]: string[] } = {
+    "Distrito Capital": ["Fiscal1 Dist1", "Fiscal2 Dist1"],
+    "Distrito Sur": ["Fiscal1 Dist2", "Fiscal2 Dist2"],
+    "Distrito Norte": ["Fiscal1 Dist3", "Fiscal2 Dist3"],
+  }
 
   constructor(private audienciaService: AudienciaService) { }
 
@@ -29,10 +46,10 @@ export class ListaAudienciasComponent implements OnInit {
 
   obtenerAudiencias() {
     this.audienciaService.getAudiencias().subscribe(
-    data => {
-      console.log(data); // Esto devuelve todas las audiencias
-      this.audiencias = data.filter(audiencia => audiencia.audEstado); // Filtra solo las activas
-      this.audienciasFiltradas = [...this.audiencias];  // ... Crea una copia nueva del array this.audiencias, en lugar de solo referenciarlo.
+      data => {
+        console.log(data); // Esto devuelve todas las audiencias
+        this.audiencias = data.filter(audiencia => audiencia.audEstado); // Filtra solo las activas
+        this.audienciasFiltradas = [...this.audiencias];  // ... Crea una copia nueva del array this.audiencias, en lugar de solo referenciarlo.
 
         // Para cada audiencia, obtener sus autoridades
         this.audiencias.forEach(audiencia => {
@@ -57,13 +74,28 @@ export class ListaAudienciasComponent implements OnInit {
     this.mostrarFormulario = !this.mostrarFormulario;
   }
 
+  actualizarDatos() {
+    this.actualizarSalas();
+    this.actualizarJueces();
+    this.actualizarFiscales();
+  }
+
   actualizarSalas() {
     this.salasDisponibles = this.salasPorDistrito[this.distritoSeleccionado] || [];
     this.salaSeleccionada = ''; // Limpia la selección de sala cuando cambia de distrito
   }
 
+  actualizarJueces() {
+    this.juecesDisponibles = this.juecesPorDistrito[this.distritoSeleccionado] || [];
+    this.juezSeleccionado = '';
+  }
+
+  actualizarFiscales() {
+    this.fiscalesDisponibles = this.fiscalesPorDistrito[this.distritoSeleccionado] || [];
+    this.fiscalSeleccionado = '';
+  }
+
   filtrarAudiencias() {
-    console.log(this.audiencias)
     this.audienciasFiltradas = this.audiencias.filter((audiencia) => {
       // Verificar si 'sal_id' y 'distrito' están definidos antes de acceder a ellos
       const coincideDistrito = this.distritoSeleccionado
@@ -84,9 +116,31 @@ export class ListaAudienciasComponent implements OnInit {
         ? audiencia.aud_fecha === this.fechaSeleccionada
         : true;
 
-      return coincideDistrito && coincideSala && coincideFecha;
+      // utiliza el operador ternario (? :), que es una forma compacta de escribir una estructura if-else en una sola línea. Equivale a
+
+      // let coincideJuez;
+      // if (this.juezSeleccionado) { 
+      //   coincideJuez = audiencia.juez === this.juezSeleccionado;
+      // } else {
+      //   coincideJuez = true;
+      // }
+
+      // Si se selecciona un juez, se compara la audiencia con ese juez. Si no se selecciona un juez, el true del "else" hace que la condición de "juez" siempre sea verdadera (es decir, no se filtra por juez).
+      const coincideJuez = this.juezSeleccionado
+        ? audiencia.juez === this.juezSeleccionado
+        : true;
+
+      const coincideFiscal = this.fiscalSeleccionado
+        ? audiencia.fiscal === this.fiscalSeleccionado
+        : true;
+
+      const coincideEstado = this.estadoSeleccionado
+        ? audiencia.aud_tipo === this.estadoSeleccionado
+        : true;
+
+
+      return coincideDistrito && coincideSala && coincideFecha && coincideJuez && coincideFiscal && coincideEstado;
     });
-    console.log(this.audienciasFiltradas);
   }
 
   borrarFiltros() {
@@ -94,6 +148,9 @@ export class ListaAudienciasComponent implements OnInit {
     this.distritoSeleccionado = '';
     this.salaSeleccionada = '';
     this.fechaSeleccionada = '';
+    this.juezSeleccionado = '';
+    this.fiscalSeleccionado = '';
+    this.estadoSeleccionado = '';
 
     // sacarle los filtros a la lista de audiencias
     this.audienciasFiltradas = [...this.audiencias];
@@ -116,6 +173,6 @@ export class ListaAudienciasComponent implements OnInit {
       });
     }
   }
-  
+
 
 }
